82ca66a80af7465e1a06810985a0aaba18e7d2f2
Carl <carl_email@163.com>
2016-02-22 01:34:06 +0800

    add files.

---
+++ QRCode/Classes/InventoryRecord/CRecordTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CRecordTableViewCell.h
+//  QRCode
+//
+//  Created by CarlLiu on 16/2/22.
+//  Copyright © 2016年 Carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRecordTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *recordLabel;
+
+@end
---
+++ QRCode/Classes/InventoryRecord/CRecordTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CRecordTableViewCell.m
+//  QRCode
+//
+//  Created by CarlLiu on 16/2/22.
+//  Copyright © 2016年 Carl. All rights reserved.
+//
+
+#import "CRecordTableViewCell.h"
+
+@implementation CRecordTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
