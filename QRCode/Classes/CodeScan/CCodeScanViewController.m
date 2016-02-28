0afc2b0a1dd03598da2e23b15ee8d242cdd4235a
Carl <carl_email@163.com>
2016-02-28 17:45:56 +0800

    submit code.

--- QRCode/Classes/CodeScan/CCodeScanViewController.m
+++ QRCode/Classes/CodeScan/CCodeScanViewController.m
@@ -53,89 +53,6 @@
             [wSelf stopScanning];
             wSelf.infoView.codeLabel.text = [NSString stringWithFormat:@"编码：%@", wSelf.codeInfo];
             [wSelf.view.layer addSublayer:wSelf.infoView.layer];
-            
-            [[CWebService sharedInstance] manual_code:wSelf.codeInfo pddw:[[CDataSource sharedInstance].loginModel pddw] success:^(NSString *obj, NSInteger code) {
-                
-                switch (code) {
-                    case 1002: {
-                        [MBProgressHUD showError:@"服务器异常"];
-                        break;
-                    }
-                    case 1006: {
-                        [MBProgressHUD showError:@"资产编号不存在"];
-                        UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"该资产不在本次清查的记录中,请确认是否标记为盘盈资产"];
-                        [alertView bk_addButtonWithTitle:@"取消" handler:^{
-                            
-                        }];
-                        [alertView bk_addButtonWithTitle:@"确定" handler:^{
-                            [[CWebService sharedInstance] manual_profit_code:wSelf.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
-                                //TODO:
-                            } failure:^(CWebServiceError *error) {
-                                [MBProgressHUD showError:error.errorMessage];
-                            } animated:YES message:@""];
-                        }];
-                        [alertView show];
-                        break;
-                    }
-                    case 2000: {
-                        if ([[obj substringToCharacter:'@'] isEqualToString:[[CDataSource sharedInstance].loginModel pddw]]) {
-                            UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"是否盘盈?"];
-                            [alertView bk_addButtonWithTitle:@"取消" handler:^{
-                                
-                            }];
-                            [alertView bk_addButtonWithTitle:@"确定" handler:^{
-                                
-                                
-                                [[CWebService sharedInstance] manual_confirm_code:wSelf.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
-                                    
-                                } failure:^(CWebServiceError *error) {
-                                    
-                                } animated:YES message:@""];
-                            }];
-                            [alertView show];
-                        } else {
-                            UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"该资产的隶属单位,与您的隶属单位不一样,请确认是否标记为您单位的盘盈资产?"];
-                            [alertView bk_addButtonWithTitle:@"取消" handler:^{
-                                
-                            }];
-                            [alertView bk_addButtonWithTitle:@"确定" handler:^{
-                                
-                            }];[alertView bk_addButtonWithTitle:@"确定" handler:^{
-                                [[CWebService sharedInstance] manual_profit_code:wSelf.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
-                                    //TODO:
-                                } failure:^(CWebServiceError *error) {
-                                    [MBProgressHUD showError:error.errorMessage];
-                                } animated:YES message:@""];
-                            }];
-                            [alertView show];
-                        }
-                        break;
-                    }
-                    case 2002: {
-                        wSelf.infoView.infoLabel.text = obj;
-                        UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"对不起,您的优先级小于1,不能二次盘点"];
-                        [alertView bk_addButtonWithTitle:@"确定" handler:^{
-                            
-                        }];
-                        [alertView show];
-                        break;
-                    }
-                    case 2003: {//对不起,您的优先级小于1,不能二次盘点.
-                        wSelf.infoView.infoLabel.text = @"页面上显示 资产不存在";
-                        UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"对不起,您的优先级小于1,不能二次盘点"];
-                        [alertView bk_addButtonWithTitle:@"确定" handler:^{
-                            
-                        }];
-                        [alertView show];
-                        break;
-                    }
-                    default:
-                        break;
-                }
-            } failure:^(CWebServiceError *error) {
-                [MBProgressHUD showError:error.errorMessage];
-                
-            } animated:YES message:@""];
         }));
         NSLog(@"Completion with result: %@", resultAsString);
         
@@ -358,6 +275,89 @@
 #pragma mark - <CCodeInfoViewDelegate>
 - (void)didCheckedButton:(UIButton *)sender {
     [self.infoView.layer removeFromSuperlayer];
+    
+    [[CWebService sharedInstance] manual_code:self.codeInfo pddw:[[CDataSource sharedInstance].loginModel pddw] success:^(NSString *obj, NSInteger code) {
+        
+        switch (code) {
+            case 1002: {
+                [MBProgressHUD showError:@"服务器异常"];
+                break;
+            }
+            case 1006: {
+                [MBProgressHUD showError:@"资产编号不存在"];
+                UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"该资产不在本次清查的记录中,请确认是否标记为盘盈资产"];
+                [alertView bk_addButtonWithTitle:@"取消" handler:^{
+                    
+                }];
+                [alertView bk_addButtonWithTitle:@"确定" handler:^{
+                    [[CWebService sharedInstance] manual_profit_code:self.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
+                        //TODO:
+                    } failure:^(CWebServiceError *error) {
+                        [MBProgressHUD showError:error.errorMessage];
+                    } animated:YES message:@""];
+                }];
+                [alertView show];
+                break;
+            }
+            case 2000: {
+                if ([[obj substringToCharacter:'@'] isEqualToString:[[CDataSource sharedInstance].loginModel pddw]]) {
+                    UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"是否盘盈?"];
+                    [alertView bk_addButtonWithTitle:@"取消" handler:^{
+                        
+                    }];
+                    [alertView bk_addButtonWithTitle:@"确定" handler:^{
+                        
+                        
+                        [[CWebService sharedInstance] manual_confirm_code:self.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
+                            
+                        } failure:^(CWebServiceError *error) {
+                            
+                        } animated:YES message:@""];
+                    }];
+                    [alertView show];
+                } else {
+                    UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"该资产的隶属单位,与您的隶属单位不一样,请确认是否标记为您单位的盘盈资产?"];
+                    [alertView bk_addButtonWithTitle:@"取消" handler:^{
+                        
+                    }];
+                    [alertView bk_addButtonWithTitle:@"确定" handler:^{
+                        
+                    }];[alertView bk_addButtonWithTitle:@"确定" handler:^{
+                        [[CWebService sharedInstance] manual_profit_code:self.codeInfo dlmc:[[CDataSource sharedInstance].loginModel dlmc] pddw:[[CDataSource sharedInstance].loginModel pddw] mc:@"人工" success:^(NSString *msg, NSInteger code) {
+                            //TODO:
+                        } failure:^(CWebServiceError *error) {
+                            [MBProgressHUD showError:error.errorMessage];
+                        } animated:YES message:@""];
+                    }];
+                    [alertView show];
+                }
+                break;
+            }
+            case 2002: {
+                self.infoView.infoLabel.text = obj;
+                UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"对不起,您的优先级小于1,不能二次盘点"];
+                [alertView bk_addButtonWithTitle:@"确定" handler:^{
+                    
+                }];
+                [alertView show];
+                break;
+            }
+            case 2003: {//对不起,您的优先级小于1,不能二次盘点.
+                self.infoView.infoLabel.text = @"页面上显示 资产不存在";
+                UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:@"对不起,您的优先级小于1,不能二次盘点"];
+                [alertView bk_addButtonWithTitle:@"确定" handler:^{
+                    
+                }];
+                [alertView show];
+                break;
+            }
+            default:
+                break;
+        }
+    } failure:^(CWebServiceError *error) {
+        [MBProgressHUD showError:error.errorMessage];
+        
+    } animated:YES message:@""];
 }
 
 @end
